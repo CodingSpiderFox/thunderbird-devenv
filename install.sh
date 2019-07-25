b58f4dde9c21aed20382c1461bc02c5ced40b0cb
CodingSpiderFox <sunrisechain@gmail.com>
2019-07-26 01:52:06 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,9 @@ sudo apt install -y mercurial libgconf2-dev
 
 hg clone https://hg.mozilla.org/releases/mozilla-esr60/ mozilla
 cd mozilla
+hg co THUNDERBIRD_60_VERBRANCH
+hg clone https://hg.mozilla.org/releases/comm-esr60/ comm
+
 python client.py checkout
 ./mach build
+./mach run
