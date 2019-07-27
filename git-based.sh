22d91b5a9449f5c9827aaac52920874b33b958f4
CodingSpiderFox <codingspiderfox@gmail.com>
2019-07-27 19:13:37 +0200

    Update git-based.sh

--- git-based.sh
+++ git-based.sh
@@ -6,6 +6,7 @@ git clone https://github.com/CodingSpiderFox/releases-comm-central comm/
 
 echo 'ac_add_options --enable-application=comm/mail' > mozconfig
 echo 'ac_add_options --enable-calendar' >> mozconfig
+echo 'ac_add_options --enable-extensions=default,venkman,inspector' >> mozconfig
 
 ./mach build
 ./mach run
