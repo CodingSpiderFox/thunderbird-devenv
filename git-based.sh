a023c4e3ba2271566eccedb2986f625d6b745181
CodingSpiderFox <codingspiderfox@gmail.com>
2019-10-10 22:23:26 +0000

    Update git-based.sh

--- git-based.sh
+++ git-based.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo apt install -y libpulse-dev
+
 git clone https://github.com/CodingSpiderFox/gecko-dev
 cd gecko-dev
 git clone https://github.com/CodingSpiderFox/releases-comm-central comm/
